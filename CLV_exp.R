dc14c52c63d49b94e4d7dc2976722c604380c0ea
ea542 <94571107+ea542@users.noreply.github.com>
2022-04-01 22:41:14 +0200

    Update CLV_exp.R

--- CLV_exp.R
+++ CLV_exp.R
@@ -1,9 +1,8 @@
 # exponencialni rozdeleni, centralni limitni veta
 
 lam = 1/5   # parametr intenzity exp. rozdeleni
-n = 510
-    # pocet scitanych velicin
-t = 100000   # pocet vzorku
+n = 510     # pocet scitanych velicin
+t = 100000  # pocet vzorku
 
 re = rowSums(matrix(rexp(n*t, lam), ncol=n)) # t vzorku souctu n exponencialnich  velicin
 
@@ -14,7 +13,7 @@ x = seq(min(re), max(re), length=200)
 # hustota normalniho rozdeleni aproximujiciho soucty pomoci CLV
 yn = dnorm(x, n/lam, (n/(lam^2))^0.5)  
 lines(x, yn, col='red')
-# presna hustota souctu (gamma rozdeleni)
+# presna hustota souctu (gama rozdeleni)
 yg = dgamma(x, shape=n, rate=lam)
 lines(x, yg, col='blue')
 legend("topright", c("aprox. CLV", "skutečná"), lty=c(1,1), col=c("red", "blue"), inset=.05, title="Hustoty pravdepodobnosti")
